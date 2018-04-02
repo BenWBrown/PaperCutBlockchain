71b0ba0cd600ee6501fc78ecc73011999c5d35eb
Ben Brown <Ben@bens-mbp-6.middlebury.edu>
2018-04-02 15:20:44 -0400

    add event listener to printer

--- PrinterClient/App.js
+++ PrinterClient/App.js
@@ -2,13 +2,31 @@ const Papercut = artifacts.require("./Papercut.sol");
 module.exports = function(callback) {
 
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(5555, () => { console.log('Example app listening on port 5555!'); console.log(Papercut); })
+app.post('/', (req, res) => {
+
+});
+
+app.listen(5555, () => {
+  let pc;
+  Papercut.deployed().then(instance => {
+    
+    // set up event listener
+    pc = instance;
+    pc.Transfer().watch( (err, resp) => {
+      if (err) {
+        console.log('err:', err);
+      } else {
+        console.log(resp.args._value.toNumber());
+      }
+    })
+  });
+})
 
 
 
