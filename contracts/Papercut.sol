fd11d7584bf658e48c0adef37be95c5d1f67c01f
Ben Brown <Ben@bens-mbp-6.middlebury.edu>
2018-04-05 21:31:17 -0400

    remove commented-out functions

--- contracts/Papercut.sol
+++ contracts/Papercut.sol
@@ -56,37 +56,6 @@ contract Papercut {
     AnnoucePrintCode(user, filehash, iv, pk1, pk2, pk3, pk4, ciphertext, mac);
   }
 
-
-  /* function print(uint8 pages, uint256 filehash) public returns(bool sufficient, uint256 balance) {
-    //TODO: MORE COMPLEX price computation. for now, 5 cents a page
-    //TODO: log request? does this with Print event i guess
-    uint price = pages * 5 * 1 ether / 385;
-    if (balances[msg.sender] < price) {
-      return (false, balances[msg.sender]);
-    }
-    balances[msg.sender] -= price;
-    Print(msg.sender, pages, filehash, balances[msg.sender]);
-    return (true, balances[msg.sender]);
-  }
-
-  function distributeCode(address user, uint64 printcode) public {
-    //TODO: think about whtether this can go straight from printer to user
-    PrintingCode(user, printcode);
-  } */
-
-  /* function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
-    if (balances[msg.sender] < amount) return false;
-    balances[msg.sender] -= amount;
-    balances[receiver] += amount;
-    Transfer(msg.sender, receiver, amount);
-    return true;
-  } */
-
-  // i dont want to import or link anything (yet?)
-  /* function getBalanceInEth(address addr) public view returns(uint){
-    return ConvertLib.convert(getBalance(addr),2);
-  } */
-
   function getBalance(address addr) public view returns(uint) {
     return balances[addr];
   }
